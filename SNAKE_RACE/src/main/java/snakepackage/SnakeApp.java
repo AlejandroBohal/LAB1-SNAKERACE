08e6bf1fe0df795568a2ba26f7e6e7b5387e5bf1
AlejandroBohal <sergio.bohorquez@mail.escuelaing.edu.co>
2020-08-17 23:21:24 -0500

    Inicializando GUI

---
+++ SNAKE_RACE/src/main/java/snakepackage/SnakeApp.java
@@ -0,0 +1,135 @@
+package snakepackage;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+import enums.GridSize;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+/**
+ * @author jd-
+ *
+ */
+public class SnakeApp {
+
+    private static SnakeApp app;
+    public static final int MAX_THREADS = 80;
+    Snake[] snakes = new Snake[MAX_THREADS];
+    private static final Cell[] spawn = {
+        new Cell(1, (GridSize.GRID_HEIGHT / 2) / 2),
+        new Cell(GridSize.GRID_WIDTH - 2,
+        3 * (GridSize.GRID_HEIGHT / 2) / 2),
+        new Cell(3 * (GridSize.GRID_WIDTH / 2) / 2, 1),
+        new Cell((GridSize.GRID_WIDTH / 2) / 2, GridSize.GRID_HEIGHT - 2),
+        new Cell(1, 3 * (GridSize.GRID_HEIGHT / 2) / 2),
+        new Cell(GridSize.GRID_WIDTH - 2, (GridSize.GRID_HEIGHT / 2) / 2),
+        new Cell((GridSize.GRID_WIDTH / 2) / 2, 1),
+        new Cell(3 * (GridSize.GRID_WIDTH / 2) / 2,
+        GridSize.GRID_HEIGHT - 2)};
+    private JFrame frame;
+    private static Board board;
+    int nr_selected = 0;
+    Thread[] thread = new Thread[MAX_THREADS];
+    JButton start = new JButton("Start");
+    JButton pause = new JButton("Pause");
+    JButton resume = new JButton("Resume");
+    public SnakeApp() {
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        frame = new JFrame("The Snake Race");
+        frame.setLayout(new BorderLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setSize(618, 640);
+        frame.setSize(GridSize.GRID_WIDTH * GridSize.WIDTH_BOX + 17,
+                GridSize.GRID_HEIGHT * GridSize.HEIGH_BOX + 40);
+        frame.setLocation(dimension.width / 2 - frame.getWidth() / 2,
+                dimension.height / 2 - frame.getHeight() / 2);
+        board = new Board();
+        frame.add(board,BorderLayout.CENTER);
+        JPanel actionsBPabel=new JPanel();
+        actionsBPabel.setLayout(new FlowLayout());
+
+
+        actionsBPabel.add(start);
+        actionsBPabel.add(pause);
+        actionsBPabel.add(resume);
+
+        frame.add(actionsBPabel,BorderLayout.SOUTH);
+        this.prepareActions();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        app = new SnakeApp();
+        app.init();
+    }
+
+    private void init() {
+        for (int i = 0; i != MAX_THREADS; i++) {
+            snakes[i] = new Snake(i + 1, spawn[i%8], i + 1);
+            snakes[i].addObserver(board);
+            thread[i] = new Thread(snakes[i]);
+        }
+        while (true) {
+            int x = 0;
+            for (int i = 0; i != MAX_THREADS; i++) {
+                if (snakes[i].isSnakeEnd() == true) {
+                    x++;
+                }
+            }
+            if (x == MAX_THREADS) {
+                break;
+            }
+        }
+        System.out.println("Thread (snake) status:");
+        for (int i = 0; i != MAX_THREADS; i++) {
+            System.out.println("["+i+"] :"+thread[i].getState());
+        }
+        
+
+    }
+
+    private void prepareActions() {
+        start.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                startThreads();
+            }
+        });
+        resume.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                resumeThreads();
+            }
+        });
+        pause.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                pauseThreads();
+            }
+        });
+    }
+
+    private void resumeThreads() {
+    }
+
+    private void pauseThreads() {
+    }
+
+    private void startThreads() {
+        for (Thread t:thread){
+            t.start();
+        }
+
+    }
+
+    public static SnakeApp getApp() {
+        return app;
+    }
+
+}
